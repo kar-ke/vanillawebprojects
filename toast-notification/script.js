bd7122363f881f98bbce13bc313626be71850583
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-07-02 23:10:50 +0530

    Toast notification component created

---
+++ toast-notification/script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const button = document.querySelector('.btn');
+    const closeButton = document.querySelector('.toast-container')
+    
+    button.addEventListener('click', function () {
+        document.querySelector('.toast-container').classList.toggle('active');
+        setTimeout(function () {
+            if (document.querySelector('.toast-container').classList.contains('active')) {
+                document.querySelector('.toast-container').classList.toggle('active');
+            }
+        }, 5000);
+    });
+    
+    document.querySelector('.close-icon').addEventListener('click', () => {
+        closeButton.classList.toggle('active');
+    });
+
+});
